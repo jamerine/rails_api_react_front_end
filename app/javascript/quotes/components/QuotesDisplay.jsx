b7120412d35c4adb8cecc0bdd91529c1bf07992e
Jason Amerine <amerine.19@gmail.com>
2017-09-19 17:57:50 -0400

    changes to jsx file

--- app/javascript/quotes/components/QuotesDisplay.jsx
+++ app/javascript/quotes/components/QuotesDisplay.jsx
@@ -1,51 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import queryString from 'query-string';
-import axios from 'axios';
-
+import React from 'react'
+import { Link } from 'react-router-dom'
+import queryString from 'query-string'
+import axios from 'axios'
 
 class QuotesDisplay extends React.Component {
   constructor () {
-    super();
+    super()
     this.state = {
       quote: {}
-    };
+    }
   }
 
   fetchQuote (id) {
-    axios.get( 'api/quotes/${id}')
+    axios.get('api/quotes/${id}')
         .then(response => {
-          this.setState({ quote: response.data });
+          this.setState({ quote: response.data })
         })
         .catch(error => {
           console.error(error)
-        });
+        })
   }
 
   setQuoteIdFromQueryString (qs) {
-    this.qsParams = queryString.parse(qs);
+    this.qsParams = queryString.parse(qs)
     if (this.qsParams.quote) {
       // assign quote ID from the URL's query string
-      this.quoteId = Number(this.qsParams.quote);
+      this.quoteId = Number(this.qsParams.quote)
     } else {
-      this.quoteId = 1;
+      this.quoteId = 1
       // update URL in browser to reflect current quote in query string
-      this.props.history.push('/?quote=${this.quoteId}');
+      this.props.history.push('/?quote=${this.quoteId}')
     }
   }
 
   componentDidMount () {
-    this.setQuoteIdFromQueryString(this.props.location.search);
-    this.fetchQuote(this.quoteId);
+    this.setQuoteIdFromQueryString(this.props.location.search)
+    this.fetchQuote(this.quoteId)
   }
 
   componentWillReceiveProps (nextProps) {
-    this.setQuoteIdFromQueryString(nextProps.location.search);
-    this.fetchQuote(this.quoteId);
+    this.setQuoteIdFromQueryString(nextProps.location.search)
+    this.fetchQuote(this.quoteId)
   }
 
   render () {
-    const nextQuoteId = Number(this.state.quote.id) + 1;
+    const nextQuoteId = Number(this.state.quote.id) + 1
 
     return (
       <div>
@@ -53,7 +52,7 @@ class QuotesDisplay extends React.Component {
         <p>{this.state.quote.text}</p>
         <p>{this.state.quote.author}</p>
       </div>
-    );
+    )
   }
 
 }
